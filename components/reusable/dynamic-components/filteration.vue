24b9cc775de6dc6af4ed39a6d445fb1ade935325
AMRWERY <amrmounir2@gmail.com>
2024-04-20 17:59:29 +0200

    ..

--- components/reusable/dynamic-components/filteration.vue
+++ components/reusable/dynamic-components/filteration.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <slot name="fulfillment" plusIcon="ant-design:plus-outlined" minusIcon="ant-design:minus-outlined" :label="$t('products.fulfillment')"></slot>
-    <slot name="categories" plusIcon="ant-design:plus-outlined" minusIcon="ant-design:minus-outlined" :label="$t('products.categories')"></slot>
-    <slot name="colors" plusIcon="ant-design:plus-outlined" minusIcon="ant-design:minus-outlined" :label="$t('products.colors')"></slot>
+    <slot name="fulfillment" plusIcon="ant-design:plus-outlined" minusIcon="ant-design:minus-outlined"
+      :label="$t('products.fulfillment')"></slot>
+    <slot name="categories" plusIcon="ant-design:plus-outlined" minusIcon="ant-design:minus-outlined"
+      :label="$t('products.categories')"></slot>
+    <slot name="colors" plusIcon="ant-design:plus-outlined" minusIcon="ant-design:minus-outlined"
+      :label="$t('products.colors')"></slot>
   </div>
 </template>
 
--- pages/carters-products/index.vue
+++ pages/carters-products/index.vue
@@ -104,7 +104,7 @@
                                                             class="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-500" />
                                                         <label for="category" class="text-sm text-gray-600 ms-3">{{
                                                             ful.label
-                                                            }}</label>
+                                                        }}</label>
                                                     </div>
                                                 </div>
                                             </HeadlessDisclosurePanel>
@@ -139,7 +139,7 @@
                                                             @change="category.checked = !category.checked; updateSelectedCategories(category.label, category.checked)" />
                                                         <label for="category" class="text-sm text-gray-600 ms-3">{{
                                                             $t(category.label)
-                                                            }}</label>
+                                                        }}</label>
                                                     </div>
                                                 </div>
                                             </HeadlessDisclosurePanel>
@@ -174,7 +174,7 @@
                                                             @change="color.checked = !color.checked; updateSelectedColor(color.label, color.checked)" />
                                                         <label for="color" class="text-sm text-gray-600 ms-3">{{
                                                             color.label
-                                                            }}</label>
+                                                        }}</label>
                                                     </div>
                                                 </div>
                                             </HeadlessDisclosurePanel>
@@ -313,7 +313,7 @@
                                                     @change="category.checked = !category.checked; updateSelectedCategories(category.label, category.checked)" />
                                                 <label for="category" class="text-sm text-gray-600 ms-3">{{
                                                     $t(category.label)
-                                                    }}</label>
+                                                }}</label>
                                             </div>
                                         </div>
                                     </HeadlessDisclosurePanel>
