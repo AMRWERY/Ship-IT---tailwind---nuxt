f6d79c2073b3c4719b2dae27810f61cf0fab09cf
AMRWERY <amrmounir2@gmail.com>
2024-11-29 16:05:31 +0200

    .

--- pages/auth/reset-password.vue
+++ pages/auth/reset-password.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="relative w-full h-screen" data-aos="fade-up" data-aos-duration="1000">
       <img src="https://justfields.com/storage/projects/7M5rV059/deal05.jpg"
-        class="absolute inset-0 object-cover w-full h-full">
+        class="absolute inset-0 object-cover w-full h-full opacity-50">
       <div class="absolute inset-0 bg-black bg-opacity-50"></div>
       <div class="absolute inset-0 flex flex-col items-center justify-center">
         <div class="w-full max-w-md py-2 mx-auto bg-white rounded-2xl">
